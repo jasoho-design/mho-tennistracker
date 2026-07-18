f9250cd033eecab62b52091003bc5f6ed4b12fc6
jasoho-design <jasoho@gmail.com>
2026-07-18 16:28:59 -0700

    Update sw.js
    bumped the cache version so his installed app actually pulls the update instead of serving the old cached copy indefinitely.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tennis-tracker-v1';
+const CACHE = 'tennis-tracker-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-180.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
